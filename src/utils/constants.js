e04cbb0d6fead3389039a3bd95805e52aabd3e47
Sangeetha priya <sangeethalaxmi18@gmail.com>
2025-05-17 19:32:37 +0530

    removed api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
-  "AIzaSyD1UjdB6U5tTV1IPw49Nu2wDficDZbxVWk";
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=";
 
 export const VIDEO_URL = "https://www.youtube.com/embed/";
 
